214216228e91b3b64c10af2aea50b9bf01cbbd37
Michael Pechner <mikey@mikey.com>
2020-10-17 22:04:38 -0700

    Added snide remark.

--- Algorithms/easy/max-sub-array.py
+++ Algorithms/easy/max-sub-array.py
@@ -1,8 +1,11 @@
 from typing import (List)
 
 class Solution:
-    # Kadane's - did not know about this method.
+    # Kadane's - did not know about this method. Was not published until I was a Junior in college.
     # https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+    # if you look in pypi - people have done the code - this is either a method,
+    # or test of what people remember from algoithms class
+    # If they took the class in the early 90's or later.
     def maxSubArray(self, nums: List[int]) -> int:
         if not nums: return 0
         curSum = maxSum = nums[0]
